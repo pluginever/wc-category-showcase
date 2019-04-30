fd5d11d11c14cd90a37d2a32ca48ca87a74635e1
MD Sultan Nasir Uddin <manikdrmc@gmail.com>
2019-04-30 23:18:14 +0600

    add promotion class

--- wc-category-showcase.php
+++ wc-category-showcase.php
@@ -33,7 +33,7 @@
 
 // don't call the file directly
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 /**
@@ -43,229 +43,232 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class WC_Category_Showcase {
 
-    /**
-     * Add-on Version
-     *
-     * @since 1.0.0
-     * @var  string
-     */
-    public $version = '1.0.7';
+	/**
+	 * Add-on Version
+	 *
+	 * @since 1.0.0
+	 * @var  string
+	 */
+	public $version = '1.0.7';
 
-    /**
-     * Initializes the class
-     *
-     * Checks for an existing instance
-     * and if it does't find one, creates it.
-     *
-     * @since 1.0.0
-     *
-     * @return object Class instance
-     */
-    public static function init() {
-        static $instance = false;
+	/**
+	 * Initializes the class
+	 *
+	 * Checks for an existing instance
+	 * and if it does't find one, creates it.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return object Class instance
+	 */
+	public static function init() {
+		static $instance = false;
 
-        if ( ! $instance ) {
-            $instance = new self();
-        }
+		if ( ! $instance ) {
+			$instance = new self();
+		}
 
-        return $instance;
-    }
+		return $instance;
+	}
 
-    /**
-     * Constructor for the class
-     *
-     * Sets up all the appropriate hooks and actions
-     *
-     * @since 1.0.0
-     *
+	/**
+	 * Constructor for the class
+	 *
+	 * Sets up all the appropriate hooks and actions
+	 *
+	 * @since 1.0.0
+	 *
 
-     */
-    public function __construct() {
-        // Localize our plugin
-        add_action( 'init', [ $this, 'localization_setup' ] );
+	 */
+	public function __construct() {
+		// Localize our plugin
+		add_action( 'init', [ $this, 'localization_setup' ] );
 
-        // on activate plugin register hook
-        register_activation_hook( __FILE__, array( $this, 'install' ) );
+		// on activate plugin register hook
+		register_activation_hook( __FILE__, array( $this, 'install' ) );
 
-        // on deactivate plugin register hook
-        register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+		// on deactivate plugin register hook
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
 
-        // Define constants
-        $this->define_constants();
+		// Define constants
+		$this->define_constants();
 
-        // Include required files
-        $this->includes();
+		// Include required files
+		$this->includes();
 
-        // Initialize the action hooks
-        $this->init_actions();
+		// Initialize the action hooks
+		$this->init_actions();
 
-        // instantiate classes
-        $this->instantiate();
+		// instantiate classes
+		$this->instantiate();
 
-        // Loaded action
-        do_action( 'wc_category_showcase' );
-    }
+		// Loaded action
+		do_action( 'wc_category_showcase' );
+	}
 
-    /**
-     * Initialize plugin for localization
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    public function localization_setup() {
-        $locale = apply_filters( 'plugin_locale', get_locale(), 'wc_category_showcase' );
-        load_textdomain( 'wc-category-showcase', WP_LANG_DIR . '/wc-category-showcase/wc-category-showcase-' . $locale . '.mo' );
-        load_plugin_textdomain( 'wc-category-showcase', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-    }
+	/**
+	 * Initialize plugin for localization
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function localization_setup() {
+		$locale = apply_filters( 'plugin_locale', get_locale(), 'wc_category_showcase' );
+		load_textdomain( 'wc-category-showcase', WP_LANG_DIR . '/wc-category-showcase/wc-category-showcase-' . $locale . '.mo' );
+		load_plugin_textdomain( 'wc-category-showcase', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
 
-    /**
-     * Executes during plugin activation
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    function install() {
-        //save install date
-        if ( false == get_option( 'wccs_install_date' ) ) {
-            update_option( 'wccs_install_date', current_time( 'timestamp' ) );
-        }
-
-
-    }
-
-    /**
-     * Executes during plugin deactivation
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    function deactivate() {
-
-    }
-
-    /**
-     * Define constants
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    private function define_constants() {
-        define( 'PLVR_WCCS_VERSION', $this->version );
-        define( 'PLVR_WCCS_FILE', __FILE__ );
-        define( 'PLVR_WCCS_PATH', dirname( PLVR_WCCS_FILE ) );
-        define( 'PLVR_WCCS_INCLUDES', PLVR_WCCS_PATH . '/includes' );
-        define( 'PLVR_WCCS_ADMIN_PATH', PLVR_WCCS_PATH . '/admin' );
-        define( 'PLVR_WCCS_ADMIN_ASSETS', plugins_url( 'admin/assets', PLVR_WCCS_ADMIN_PATH ) );
-        define( 'PLVR_WCCS_URL', plugins_url( '', PLVR_WCCS_FILE ) );
-        define( 'PLVR_WCCS_ASSETS', PLVR_WCCS_URL . '/assets' );
-        define( 'PLVR_WCCS_VIEWS', PLVR_WCCS_PATH . '/views' );
-        define( 'PLVR_WCCS_TEMPLATES_DIR', PLVR_WCCS_PATH . '/templates' );
-    }
-
-    /**
-     * Include required files
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    private function includes() {
-        require PLVR_WCCS_INCLUDES . '/functions.php';
-        require PLVR_WCCS_INCLUDES . '/custom-cp.php';
-        require PLVR_WCCS_INCLUDES . '/class-shortcode.php';
-        require PLVR_WCCS_INCLUDES . '/metabox/class-metabox.php';
-        require PLVR_WCCS_ADMIN_PATH . '/class-admin.php';
-        require PLVR_WCCS_ADMIN_PATH . '/class-metabox.php';
-        require PLVR_WCCS_INCLUDES . '/class-insights.php';
-        require PLVR_WCCS_INCLUDES . '/class-tracker.php';
-    }
-
-    /**
-     * Do plugin upgrades
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    function plugin_upgrades() {
-
-        if ( ! current_user_can( 'manage_options' ) ) {
-            return;
-        }
-
-        require_once PLVR_WCCS_INCLUDES . '/class-upgrades.php';
-
-        $upgrader = new WCCS_Upgrades();
-
-        if ( $upgrader->needs_update() ) {
-            $upgrader->perform_updates();
-        }
-    }
-
-    /**
-     * Init Hooks
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    private function init_actions() {
-        add_action( 'wp_enqueue_scripts', array( $this, 'load_assets' ) );
-        add_action( 'admin_init', array( $this, 'plugin_upgrades' ) );
-        add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( __CLASS__, 'plugin_action_links' ) );
-
-    }
-
-    /**
-     * Instantiate classes
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    private function instantiate() {
-        new \Pluginever\WCCS\Shortcode();
-        new \Pluginever\WCCCS\Metabox();
-        new \Pluginever\WCCS\Tracker();
-    }
-
-    /**
-     * Add all the assets required by the plugin
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    function load_assets() {
-        $suffix = ( defined( 'WP_DEBUG' ) && WP_DEBUG ) ? '' : '.min';
-        wp_register_style( 'wc-category-showcase', PLVR_WCCS_ASSETS . "/css/wc-category-showcase{$suffix}.css", [], date( 'i' ) );
-        wp_register_script( 'wc-category-showcase', PLVR_WCCS_ASSETS . "/js/wc-category-showcase{$suffix}.js", [ 'jquery' ], date( 'i' ), true );
-        wp_localize_script( 'wc-category-showcase', 'jsobject', [ 'ajaxurl' => admin_url( 'admin-ajax.php' ) ] );
-        wp_enqueue_style( 'wc-category-showcase' );
-        wp_enqueue_script( 'wc-category-showcase' );
-    }
+	/**
+	 * Executes during plugin activation
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	function install() {
+		//save install date
+		if ( false == get_option( 'wccs_install_date' ) ) {
+			update_option( 'wccs_install_date', current_time( 'timestamp' ) );
+		}
 
 
-    /**
-     * @param $links
-     *
-     * @return array
-     */
-    public static function plugin_action_links( $links ) {
-        $doc_link     = 'https://www.pluginever.com/docs/woocommerce-category-showcase/';
-        $action_links = [];
-        if ( ! wc_category_showcase_is_pro_active() ) {
-            $action_links['Upgrade'] = '<a target="_blank" href="https://www.pluginever.com/plugins/woocommerce-category-showcase-pro/" title="' . esc_attr( __( 'Upgrade To Pro', 'wccs' ) ) . '" style="color:red;font-weight:bold;">' . __( 'Upgrade To Pro', 'woocatlider' ) . '</a>';
-        }
-        $action_links['Documentation'] = '<a target="_blank" href="' . $doc_link . '" title="' . esc_attr( __( 'View Plugin\'s Documentation', 'wccs' ) ) . '">' . __( 'Documentation', 'wccs' ) . '</a>';
+	}
+
+	/**
+	 * Executes during plugin deactivation
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	function deactivate() {
+
+	}
+
+	/**
+	 * Define constants
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	private function define_constants() {
+		define( 'PLVR_WCCS_VERSION', $this->version );
+		define( 'PLVR_WCCS_FILE', __FILE__ );
+		define( 'PLVR_WCCS_PATH', dirname( PLVR_WCCS_FILE ) );
+		define( 'PLVR_WCCS_INCLUDES', PLVR_WCCS_PATH . '/includes' );
+		define( 'PLVR_WCCS_ADMIN_PATH', PLVR_WCCS_PATH . '/admin' );
+		define( 'PLVR_WCCS_ADMIN_ASSETS', plugins_url( 'admin/assets', PLVR_WCCS_ADMIN_PATH ) );
+		define( 'PLVR_WCCS_URL', plugins_url( '', PLVR_WCCS_FILE ) );
+		define( 'PLVR_WCCS_ASSETS', PLVR_WCCS_URL . '/assets' );
+		define( 'PLVR_WCCS_VIEWS', PLVR_WCCS_PATH . '/views' );
+		define( 'PLVR_WCCS_TEMPLATES_DIR', PLVR_WCCS_PATH . '/templates' );
+	}
+
+	/**
+	 * Include required files
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	private function includes() {
+		require PLVR_WCCS_INCLUDES . '/functions.php';
+		require PLVR_WCCS_INCLUDES . '/custom-cp.php';
+		require PLVR_WCCS_INCLUDES . '/class-shortcode.php';
+		require PLVR_WCCS_INCLUDES . '/metabox/class-metabox.php';
+		require PLVR_WCCS_ADMIN_PATH . '/class-admin.php';
+		require PLVR_WCCS_ADMIN_PATH . '/class-metabox.php';
+		require PLVR_WCCS_INCLUDES . '/class-insights.php';
+		require PLVR_WCCS_INCLUDES . '/class-tracker.php';
+		if(is_admin()){
+			require PLVR_WCCS_INCLUDES . '/class-promotion.php';
+		}
+	}
+
+	/**
+	 * Do plugin upgrades
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	function plugin_upgrades() {
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		require_once PLVR_WCCS_INCLUDES . '/class-upgrades.php';
+
+		$upgrader = new WCCS_Upgrades();
+
+		if ( $upgrader->needs_update() ) {
+			$upgrader->perform_updates();
+		}
+	}
+
+	/**
+	 * Init Hooks
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	private function init_actions() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'load_assets' ) );
+		add_action( 'admin_init', array( $this, 'plugin_upgrades' ) );
+		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( __CLASS__, 'plugin_action_links' ) );
+
+	}
+
+	/**
+	 * Instantiate classes
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	private function instantiate() {
+		new \Pluginever\WCCS\Shortcode();
+		new \Pluginever\WCCCS\Metabox();
+		new \Pluginever\WCCS\Tracker();
+	}
+
+	/**
+	 * Add all the assets required by the plugin
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	function load_assets() {
+		$suffix = ( defined( 'WP_DEBUG' ) && WP_DEBUG ) ? '' : '.min';
+		wp_register_style( 'wc-category-showcase', PLVR_WCCS_ASSETS . "/css/wc-category-showcase{$suffix}.css", [], date( 'i' ) );
+		wp_register_script( 'wc-category-showcase', PLVR_WCCS_ASSETS . "/js/wc-category-showcase{$suffix}.js", [ 'jquery' ], date( 'i' ), true );
+		wp_localize_script( 'wc-category-showcase', 'jsobject', [ 'ajaxurl' => admin_url( 'admin-ajax.php' ) ] );
+		wp_enqueue_style( 'wc-category-showcase' );
+		wp_enqueue_script( 'wc-category-showcase' );
+	}
 
 
-        return array_merge( $action_links, $links );
-    }
+	/**
+	 * @param $links
+	 *
+	 * @return array
+	 */
+	public static function plugin_action_links( $links ) {
+		$doc_link     = 'https://www.pluginever.com/docs/woocommerce-category-showcase/';
+		$action_links = [];
+		if ( ! wc_category_showcase_is_pro_active() ) {
+			$action_links['Upgrade'] = '<a target="_blank" href="https://www.pluginever.com/plugins/woocommerce-category-showcase-pro/" title="' . esc_attr( __( 'Upgrade To Pro', 'wccs' ) ) . '" style="color:red;font-weight:bold;">' . __( 'Upgrade To Pro', 'woocatlider' ) . '</a>';
+		}
+		$action_links['Documentation'] = '<a target="_blank" href="' . $doc_link . '" title="' . esc_attr( __( 'View Plugin\'s Documentation', 'wccs' ) ) . '">' . __( 'Documentation', 'wccs' ) . '</a>';
+
+
+		return array_merge( $action_links, $links );
+	}
 
 }
 
@@ -276,7 +279,7 @@ $GLOBALS['WC_Category_Showcase'] = new WC_Category_Showcase();
  * Grab the $WC_Category_Showcase object and return it
  */
 function wc_category_showcase() {
-    global $WC_Category_Showcase;
+	global $WC_Category_Showcase;
 
-    return $WC_Category_Showcase;
+	return $WC_Category_Showcase;
 }
