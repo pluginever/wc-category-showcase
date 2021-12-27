4932075fc3c90092083eb4202f217def7e000732
Sudipto Shakhari <Sudipto004.ruet@gmail.com>
2021-12-27 15:55:57 +0600

    plugin version updated in main file

--- wc-category-showcase.php
+++ wc-category-showcase.php
@@ -3,16 +3,16 @@
  * Plugin Name: WooCommerce Category Showcase
  * Plugin URI:  https://pluginever.com/wc-category-showcase
  * Description: WooCommerce extension to showcase categories in a nice slider blocks
- * Version:     1.1.4
+ * Version:     1.1.6
  * Author:      PluginEver
  * Author URI:  https://pluginever.com
  * License:     GPLv2+
  * Text Domain: wc-category-showcase
  * Domain Path: /i18n/languages
  * Requires at least: 4.4
- * Tested up to: 5.6
+ * Tested up to: 5.8.2
  * WC requires at least: 3.0.0
- * WC tested up to: 4.9.1
+ * WC tested up to: 6.0.0
  */
 
 /**
@@ -51,7 +51,7 @@ class WC_Category_Showcase {
 	 * @since 1.0.0
 	 * @var  string
 	 */
-	public $version = '1.1.4';
+	public $version = '1.1.6';
 
 	/**
 	 * admin notices
