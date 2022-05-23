5e417d110a7e27476d7f9657363606abb2ddd75c
Sudipto Shakhari <Sudipto004.ruet@gmail.com>
2022-05-23 16:35:40 +0600

    update menu title

--- includes/custom-cp.php
+++ includes/custom-cp.php
@@ -8,9 +8,9 @@ add_action( 'init', 'wccs_custom_post_type' );
 
 function wccs_custom_post_type() {
 	$labels = array(
-		'name'               => _x( 'WC Category Showcase', 'post type general name', 'wc-category-showcase' ),
+		'name'               => _x( 'Category Showcase', 'post type general name', 'wc-category-showcase' ),
 		'singular_name'      => _x( 'WC Category Showcase', 'post type singular name', 'wc-category-showcase' ),
-		'menu_name'          => _x( 'WC Showcase', 'admin menu', 'wc-category-showcase' ),
+		'menu_name'          => _x( 'Category Showcase', 'admin menu', 'wc-category-showcase' ),
 		'name_admin_bar'     => _x( 'WC Category Showcase', 'add new on admin bar', 'wc-category-showcase' ),
 		'add_new'            => _x( 'Add New', 'book', 'wc-category-showcase' ),
 		'add_new_item'       => __( 'Add New Category Showcase', 'wc-category-showcase' ),
@@ -20,8 +20,8 @@ function wccs_custom_post_type() {
 		'all_items'          => __( 'All Category Showcases', 'wc-category-showcase' ),
 		'search_items'       => __( 'Search Showcase', 'wc-category-showcase' ),
 		'parent_item_colon'  => __( 'Parent Showcase:', 'wc-category-showcase' ),
-		'not_found'          => __( 'No Showcase found.', 'wc-category-showcase' ),
-		'not_found_in_trash' => __( 'No Showcase found in Trash.', 'wc-category-showcase' )
+		'not_found'          => __( 'No showcase found.', 'wc-category-showcase' ),
+		'not_found_in_trash' => __( 'No showcase found in trash.', 'wc-category-showcase' )
 	);
 
 	$args = array(
