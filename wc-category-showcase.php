73a5c3d1a794b8c6864c67f5ad1361a24da7c4c6
Sultan Nasir Uddin <manikdrmc@gmail.com>
2022-11-20 15:25:13 +0600

    Update wc-category-showcase.php
    Co-authored-by: Bappi <bappi.cs@gmail.com>

--- wc-category-showcase.php
+++ wc-category-showcase.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Category Showcase for WooCommerce
- * Plugin URI:  https://pluginever.com/wc-category-showcase
+ * Plugin URI: https://pluginever.com/plugins/woocommerce-category-showcase-pro/
  * Description: WooCommerce extension to showcase categories in a nice slider blocks
  * Version:     1.1.9
  * Author:      PluginEver
