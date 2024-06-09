9bb6a7eb63e51bd1c6413b61a18a80bcf0cdba52
devsabbirhossain <sabbir2dev@gmail.com>
2024-06-09 16:13:36 +0600

    update settings functions

--- includes/Shortcodes/Shortcodes.php
+++ includes/Shortcodes/Shortcodes.php
@@ -52,11 +52,11 @@ class Shortcodes {
 		$wccs_id  = intval( $atts['id'] );
 		$showcase = Helpers::get_slider_settings( $wccs_id );
 
-		$layout        = isset( $showcase['wcc_showcase_layout'] ) ? $showcase['wcc_showcase_layout'] : 'slider';
+		$layout        = isset( $showcase['layout'] ) ? $showcase['layout'] : 'slider';
 		$layout_option = '';
 
 		if ( 'block' === $layout ) {
-			$layout_option = isset( $showcase['wcc_showcase_block_column'] ) ? 'column__x' . $showcase['wcc_showcase_block_column'] : 'column__x1';
+			$layout_option = isset( $showcase['block_column'] ) ? 'column__x' . $showcase['block_column'] : 'column__x1';
 		}
 		if ( 'grid' === $layout ) {
 			// Available options are:
@@ -66,16 +66,16 @@ class Shortcodes {
 			// 5X: catalog_1x5, catalog_2x5, schema_1x5, schema_2x5, modern_1x5, modern_2x5, modern_3x5.
 			// 6X: catalog_1x6, catalog_2x6, matrix_1x6, matrix_2x6, zen_1x6, zen_2x6, zen_3x6.
 			// 7X: catalog_1x7, catalog_2x7, helix_1x7, helix_2x7.
-			$layout_option = isset( $showcase['wcc_showcase_layout_option'] ) ? sanitize_key( $showcase['wcc_showcase_layout_option'] ) : '';
+			$layout_option = isset( $showcase['layout_option'] ) ? sanitize_key( $showcase['layout_option'] ) : '';
 		}
 
 		// Get the showcase individual style.
-		$card_bg_color         = $showcase['wcc_showcase_card']['background_color'] ?? '#ffffff';
-		$card_bg_hover_color   = $showcase['wcc_showcase_card']['hover_color'] ?? '#cccccc';
-		$card_text_color       = $showcase['wcc_showcase_card']['text_color'] ?? '#000000';
-		$card_text_hover_color = $showcase['wcc_showcase_card']['hover_text_color'] ?? '#cccccc';
-		$card_border_radius    = $showcase['wcc_showcase_border_radius'] ?? '8';
-		$card_gap              = $showcase['wcc_showcase_gap_between_cards'] ?? '9';
+		$card_bg_color         = $showcase['card']['background_color'] ?? '#ffffff';
+		$card_bg_hover_color   = $showcase['card']['hover_color'] ?? '#cccccc';
+		$card_text_color       = $showcase['card']['text_color'] ?? '#000000';
+		$card_text_hover_color = $showcase['card']['hover_text_color'] ?? '#cccccc';
+		$card_border_radius    = $showcase['border_radius'] ?? '8';
+		$card_gap              = $showcase['gap_between_cards'] ?? '9';
 
 		$styles = "
 			.wccs-categories__{$wccs_id}{
@@ -98,11 +98,11 @@ class Shortcodes {
 
 		?>
 		<section class="wccs-section is-layout__<?php echo sanitize_html_class( $layout ); ?>">
-			<?php if ( isset( $showcase['wcc_showcase_section_title'] ) || isset( $showcase['wcc_showcase_section_description'] ) ) : ?>
-				<div class="wccs-section__header text-<?php echo isset( $showcase['wcc_showcase_heading_alignment'] ) ? sanitize_html_class( $showcase['wcc_showcase_heading_alignment'] ) : 'left'; ?>">
+			<?php if ( isset( $showcase['section_title'] ) || isset( $showcase['section_description'] ) ) : ?>
+				<div class="wccs-section__header text-<?php echo isset( $showcase['heading_alignment'] ) ? sanitize_html_class( $showcase['heading_alignment'] ) : 'left'; ?>">
 					<?php
-					echo isset( $showcase['wcc_showcase_section_title'] ) ? '<h2>' . esc_html( $showcase['wcc_showcase_section_title'] ) . '</h2>' : '';
-					echo isset( $showcase['wcc_showcase_section_description'] ) ? '<p>' . esc_html( $showcase['wcc_showcase_section_description'] ) . '</p>' : '';
+					echo isset( $showcase['section_title'] ) ? '<h2>' . esc_html( $showcase['section_title'] ) . '</h2>' : '';
+					echo isset( $showcase['section_description'] ) ? '<p>' . esc_html( $showcase['section_description'] ) . '</p>' : '';
 					?>
 				</div>
 			<?php endif; ?>
@@ -135,16 +135,16 @@ class Shortcodes {
 	 */
 	public static function get_content_html( $wccs_id, $layout, $showcase ) {
 
-		$content_placement = $showcase['wcc_showcase_content_placement'] ?? 'top';
-		$content_position  = $showcase['wcc_showcase_overlay_content_position'] ?? 'center_center';
+		$content_placement = $showcase['content_placement'] ?? 'top';
+		$content_position  = $showcase['overlay_content_position'] ?? 'center_center';
 		$thumbnail_img     = WC_CATEGORY_SHOWCASE_ASSETS_URL . 'images/category-placeholder-img1.png';
 
-		$count = isset( $showcase['wcc_showcase_block_column'] ) ? absint( $showcase['wcc_showcase_block_column'] ) : null;
+		$count = isset( $showcase['block_column'] ) ? absint( $showcase['block_column'] ) : null;
 
-		if ( 'all' === $showcase['wcc_showcase_category_filter'] ) {
+		if ( 'all' === $showcase['category_filter'] ) {
 			$categories = Helpers::get_all_categories( $count );
 		} else {
-			$categories = isset( $category_showcase['wcc_showcase_specific_category_select'] ) ? $category_showcase['wcc_showcase_specific_category_select'] : array();
+			$categories = isset( $category_showcase['specific_category_select'] ) ? $category_showcase['specific_category_select'] : array();
 		}
 
 		foreach ( $categories as $category ) {
@@ -187,30 +187,30 @@ class Shortcodes {
 
 		$post_id           = $wccs_id;
 		$category_showcase = $showcase;
-		if ( 'all' === $category_showcase['wcc_showcase_category_filter'] ) {
+		if ( 'all' === $category_showcase['category_filter'] ) {
 			$categories = Helpers::get_all_categories();
 		} else {
-			$categories = isset( $category_showcase['wcc_showcase_specific_category_select'] ) ? $category_showcase['wcc_showcase_specific_category_select'] : array();
+			$categories = isset( $category_showcase['specific_category_select'] ) ? $category_showcase['specific_category_select'] : array();
 		}
 		$slider_class_list = $class_list;
 		$slider_config     = $config;
-		$is_ticker         = array_key_exists( 'wcc_showcase_slide_is_ticker', $category_showcase ) && 'yes' === $category_showcase['wcc_showcase_slide_is_ticker'] ? 'true' : 'false';
-		$ticker_direction  = 'right_to_left' === $category_showcase['wcc_showcase_slider_ticker_direction'] ? 'true' : 'false';
-		$ticker_mode       = 'medium' === $category_showcase['wcc_showcase_ticket_mode'] ? 1 : ( 'slow' === $category_showcase['wcc_showcase_ticket_mode'] ? .5 : 2 );
+		$is_ticker         = array_key_exists( 'slide_is_ticker', $category_showcase ) && 'yes' === $category_showcase['slide_is_ticker'] ? 'true' : 'false';
+		$ticker_direction  = 'right_to_left' === $category_showcase['slider_ticker_direction'] ? 'true' : 'false';
+		$ticker_mode       = 'medium' === $category_showcase['ticket_mode'] ? 1 : ( 'slow' === $category_showcase['ticket_mode'] ? .5 : 2 );
 
 		// Added latter.
 		$content_placement = $showcase['content_placement'] ?? 'bottom';
 		$content_position  = $category_showcase['slide_content_position'] ?? 'center_center';
 		?>
 
-		<div class="splide wcc-showcase-<?php echo esc_attr( $post_id ); ?> <?php echo esc_attr( $slider_class_list ); ?>" id="wcc-showcase-<?php echo esc_attr( $post_id ); ?>" data-splide='<?php echo esc_attr( $slider_config ); ?>' data-ticker='{"isTicker":<?php echo esc_attr( $is_ticker ); ?>, "tickerDirection":<?php echo esc_attr( $ticker_direction ); ?>, "tickerSpeed":<?php echo esc_attr( $ticker_mode ); ?>}' data-grid='{"rows": <?php echo esc_attr( $category_showcase['wcc_showcase_slider']['row'] ); ?>, "columns": <?php echo esc_attr( $category_showcase['wcc_showcase_slider']['column'] ); ?>, "laptop":<?php echo esc_attr( $category_showcase['wcc_showcase_column_breakpoint']['laptop'] ); ?>, "tablet":<?php echo esc_attr( $category_showcase['wcc_showcase_column_breakpoint']['tablet'] ); ?>, "mobile":<?php echo esc_attr( $category_showcase['wcc_showcase_column_breakpoint']['mobile'] ); ?> }' aria-label="<?php echo esc_attr( get_the_title( $post_id ) ); ?>">
+		<div class="splide wcc-showcase-<?php echo esc_attr( $post_id ); ?> <?php echo esc_attr( $slider_class_list ); ?>" id="wcc-showcase-<?php echo esc_attr( $post_id ); ?>" data-splide='<?php echo esc_attr( $slider_config ); ?>' data-ticker='{"isTicker":<?php echo esc_attr( $is_ticker ); ?>, "tickerDirection":<?php echo esc_attr( $ticker_direction ); ?>, "tickerSpeed":<?php echo esc_attr( $ticker_mode ); ?>}' data-grid='{"rows": <?php echo esc_attr( $category_showcase['slider']['row'] ); ?>, "columns": <?php echo esc_attr( $category_showcase['slider']['column'] ); ?>, "laptop":<?php echo esc_attr( $category_showcase['column_breakpoint']['laptop'] ); ?>, "tablet":<?php echo esc_attr( $category_showcase['column_breakpoint']['tablet'] ); ?>, "mobile":<?php echo esc_attr( $category_showcase['column_breakpoint']['mobile'] ); ?> }' aria-label="<?php echo esc_attr( get_the_title( $post_id ) ); ?>">
 					<div class="splide__track">
 						<ul class="splide__list ajsFCDwhesvcusds">
 							<?php
 							foreach ( $categories as $category_id ) {
 								$category_details = Helpers::get_category_details( $category_id )
 								?>
-								<li class="splide__slide wcc-showcase-slide-item wccs-content__<?php echo sanitize_html_class( $content_placement ); ?>" data-splide-interval="<?php echo esc_attr( $category_showcase['wcc_showcase_slide_speed'] ); ?>">
+								<li class="splide__slide wcc-showcase-slide-item wccs-content__<?php echo sanitize_html_class( $content_placement ); ?>" data-splide-interval="<?php echo esc_attr( $category_showcase['slide_speed'] ); ?>">
 									<div class="wcc-showcase-slide-item__cat-thumbnails">
 										<div class="wcc-showcase-slide-item__cat-thumbnails__image">
 											<a href="<?php echo esc_url( $category_details['cat_link'] ); ?>">
@@ -220,29 +220,29 @@ class Shortcodes {
 									</div>
 									<div class="wcc-showcase-slide-item__cat-details wccs-content-position__<?php echo sanitize_html_class( $content_position ); ?>">
 										<div class="wccs-entry__content-inner">
-											<?php if ( 'yes' === $category_showcase['wcc_showcase_show_category_icon'] ) { ?>
+											<?php if ( 'yes' === $category_showcase['show_category_icon'] ) { ?>
 												<div class="wcc-showcase-slide-item__cat-thumbnails__icon">
 													<a href="<?php echo esc_url( $category_details['cat_link'] ); ?>">
 														<img  src="<?php echo esc_url( $category_details['icon_url'] ); ?>" alt="<?php echo esc_attr( $category_details['slug'] ); ?>">
 													</a>
 												</div>
 											<?php } ?>
-											<?php if ( 'yes' === $category_showcase['wcc_showcase_show_category_title'] ) { ?>
+											<?php if ( 'yes' === $category_showcase['show_category_title'] ) { ?>
 												<div class="wcc-showcase-slide-item__cat-title">
 													<h3><a href="<?php echo esc_url( $category_details['cat_link'] ); ?>"><?php echo esc_attr( $category_details['name'] ); ?></a></h3>
 												</div>
 											<?php } ?>
-											<?php if ( 'yes' === $category_showcase['wcc_showcase_show_category_description'] ) { ?>
+											<?php if ( 'yes' === $category_showcase['show_category_description'] ) { ?>
 												<div class="wcc-showcase-slide-item__cat-description">
 													<p><?php echo esc_attr( $category_details['description'] ); ?></p>
 												</div>
 											<?php } ?>
-											<?php if ( 'yes' === $category_showcase['wcc_showcase_show_category_product_quantity'] ) { ?>
+											<?php if ( 'yes' === $category_showcase['show_category_product_quantity'] ) { ?>
 												<div class="wcc-showcase-slide-item__cat-products">
 													<a href="<?php echo esc_url( $category_details['cat_link'] ); ?>"><?php printf( '%s Products', esc_attr( $category_details['total_count'] ) ); ?></a>
 												</div>
 											<?php } ?>
-											<?php if ( ! empty( $category_details['child_categories'] && 'yes' === $category_showcase['wcc_showcase_show_subcategory_product_quantity'] ) ) { ?>
+											<?php if ( ! empty( $category_details['child_categories'] && 'yes' === $category_showcase['show_subcategory_product_quantity'] ) ) { ?>
 												<div class="wcc-showcase-slide-item__sub-cat">
 													<ul class="wcc-showcase-slide-item__sub-cat__sub-cat-list">
 														<?php foreach ( $category_details['child_categories'] as $child_category ) { ?>
@@ -251,17 +251,17 @@ class Shortcodes {
 													</ul>
 												</div>
 											<?php } ?>
-											<?php if ( 'yes' === $category_showcase['wcc_showcase_show_custom_text'] ) { ?>
+											<?php if ( 'yes' === $category_showcase['show_custom_text'] ) { ?>
 												<div class="wcc-showcase-slide-item__cat-custom-text">
 													<p><?php echo esc_attr( $category_details['custom_text'] ); ?></p>
 												</div>
 											<?php } ?>
-											<?php if ( 'yes' === $category_showcase['wcc_showcase_show_button'] ) { ?>
+											<?php if ( 'yes' === $category_showcase['show_button'] ) { ?>
 												<div class="wcc-showcase-slide-item__cat-button">
 													<a href="<?php echo esc_attr( $category_details['cat_link'] ); ?>" class="btn wccs-showcase-btn">
 														<?php
-														echo esc_attr( $category_showcase['wcc_showcase_button_text'] );
-														if ( 'yes' === $category_showcase['wcc_showcase_show_button_icon'] ) {
+														echo esc_attr( $category_showcase['button_text'] );
+														if ( 'yes' === $category_showcase['show_button_icon'] ) {
 															?>
 															<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
 																<path d="M14.4192 6.35859C14.1263 6.06569 13.6515 6.06569 13.3586 6.35859C13.0657 6.65148 13.0657 7.12635 13.3586 7.41925L14.4192 6.35859ZM17 10L17.5303 10.5304C17.8232 10.2375 17.8232 9.76259 17.5303 9.4697L17 10ZM13.3586 12.5808C13.0657 12.8737 13.0657 13.3486 13.3586 13.6415C13.6515 13.9344 14.1263 13.9344 14.4192 13.6415L13.3586 12.5808ZM3 9.25003C2.58579 9.25003 2.25 9.58581 2.25 10C2.25 10.4142 2.58579 10.75 3 10.75L3 9.25003ZM13.3586 7.41925L16.4697 10.5304L17.5303 9.4697L14.4192 6.35859L13.3586 7.41925ZM16.4697 9.4697L13.3586 12.5808L14.4192 13.6415L17.5303 10.5304L16.4697 9.4697ZM17 9.25003L3 9.25003L3 10.75L17 10.75V9.25003Z"/>
@@ -298,18 +298,18 @@ class Shortcodes {
 		$slider_config = '{
 		"perPage": 1,
 		"perMove": 1,
-		"type":"' . ( 'yes' === $category_showcase['wcc_showcase_slide_unlimited_loop'] ? 'loop' : 'slide' ) . '",
+		"type":"' . ( 'yes' === $category_showcase['slide_unlimited_loop'] ? 'loop' : 'slide' ) . '",
 		"gap":"20px",
-		"autoplay": ' . ( 'yes' === $category_showcase['wcc_showcase_slide_slideshow'] ? 'true' : 'false' ) . ',
-		"arrows": ' . ( 'yes' === $category_showcase['wcc_showcase_slide_navigation_arrow'] ? 'true' : 'false' ) . ',
-		"pagination": ' . ( 'yes' === $category_showcase['wcc_showcase_slide_show_counter'] ? 'true' : 'false' ) . ',
+		"autoplay": ' . ( 'yes' === $category_showcase['slide_slideshow'] ? 'true' : 'false' ) . ',
+		"arrows": ' . ( 'yes' === $category_showcase['slide_navigation_arrow'] ? 'true' : 'false' ) . ',
+		"pagination": ' . ( 'yes' === $category_showcase['slide_show_counter'] ? 'true' : 'false' ) . ',
 		"rewind": true,
-		"rewindSpeed": ' . ( ! empty( $category_showcase['wcc_showcase_slide_speed'] ) ? $category_showcase['wcc_showcase_slide_speed'] : '3000' ) . ',
+		"rewindSpeed": ' . ( ! empty( $category_showcase['slide_speed'] ) ? $category_showcase['slide_speed'] : '3000' ) . ',
 		"speed": 1000,
-		"interval": ' . ( ! empty( $category_showcase['wcc_showcase_slide_speed'] ) ? $category_showcase['wcc_showcase_slide_speed'] : '3000' ) . ',
-		"drag":' . ( 'yes' === $category_showcase['wcc_showcase_slide_draggable_slide'] ? 'true' : 'false' ) . ',
-		"pauseOnHover":' . ( 'yes' === $category_showcase['wcc_showcase_slide_stop_on_hover'] ? 'true' : 'false' ) . ',
-		"releaseWheel":' . ( 'yes' === $category_showcase['wcc_showcase_slide_scroll_interaction'] ? 'true' : 'false' ) . ',
+		"interval": ' . ( ! empty( $category_showcase['slide_speed'] ) ? $category_showcase['slide_speed'] : '3000' ) . ',
+		"drag":' . ( 'yes' === $category_showcase['slide_draggable_slide'] ? 'true' : 'false' ) . ',
+		"pauseOnHover":' . ( 'yes' === $category_showcase['slide_stop_on_hover'] ? 'true' : 'false' ) . ',
+		"releaseWheel":' . ( 'yes' === $category_showcase['slide_scroll_interaction'] ? 'true' : 'false' ) . ',
 		"direction": "ltr",
 		"wheel":false
 		}';
@@ -325,19 +325,19 @@ class Shortcodes {
 	 */
 	protected function get_slider_classes( $category_showcase ) {
 		$slider_classes = '';
-		if ( 'yes' === $category_showcase['wcc_showcase_slide_navigation_arrow'] ) {
+		if ( 'yes' === $category_showcase['slide_navigation_arrow'] ) {
 			$slider_classes .= 'is--arrow';
-			$slider_classes .= 'arrow' === $category_showcase['wcc_showcase_slide_arrow_style'] ? '' : ' is--navigation-chevron';
-			$slider_classes .= 'default' === $category_showcase['wcc_showcase_slider_navigation_position'] ? '' : ' is--navigation-' . $category_showcase['wcc_showcase_slider_navigation_position'];
+			$slider_classes .= 'arrow' === $category_showcase['slide_arrow_style'] ? '' : ' is--navigation-chevron';
+			$slider_classes .= 'default' === $category_showcase['slider_navigation_position'] ? '' : ' is--navigation-' . $category_showcase['slider_navigation_position'];
 		}
 
-		if ( 'yes' === $category_showcase['wcc_showcase_slide_show_counter'] ) {
+		if ( 'yes' === $category_showcase['slide_show_counter'] ) {
 			$slider_classes .= ' is-pagination';
-			$slider_classes .= 'dots' === $category_showcase['wcc_showcase_slide_counter_style'] ? ' ' : ( 'dashes' === $category_showcase['wcc_showcase_slide_counter_style'] ? ' is--pagination-only-dashes' : ' is--pagination-dash-with-dots' );
+			$slider_classes .= 'dots' === $category_showcase['slide_counter_style'] ? ' ' : ( 'dashes' === $category_showcase['slide_counter_style'] ? ' is--pagination-only-dashes' : ' is--pagination-dash-with-dots' );
 		}
 
-		if ( 'yes' === $category_showcase['wcc_showcase_show_button'] ) {
-			$slider_classes .= ' is-button-' . $category_showcase['wcc_showcase_button_style'];
+		if ( 'yes' === $category_showcase['show_button'] ) {
+			$slider_classes .= ' is-button-' . $category_showcase['button_style'];
 		}
 		return $slider_classes;
 	}
