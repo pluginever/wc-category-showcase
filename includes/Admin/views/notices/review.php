ace8f1ec8f54414f060ed7165cf3dbe10054179d
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2025-10-20 16:33:43 +0600

    Prepare Release v2.2.6

--- includes/Admin/views/notices/review.php
+++ includes/Admin/views/notices/review.php
@@ -13,19 +13,19 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <div class="notice-body">
 	<div class="notice-icon">
-		<img src="<?php echo esc_attr( wc_category_showcase()->get_assets_url( 'images/plugin-icon.png' ) ); ?>" alt="WC Category Showcase">
+		<img src="<?php echo esc_attr( wc_category_showcase()->get_assets_url( 'images/plugin-icon.png' ) ); ?>" alt="Product Category Showcase for WooCommerce">
 	</div>
 	<div class="notice-content">
 		<h3>
-			<?php esc_html_e( 'Enjoying WC Category Showcase?', 'wc-category-showcase' ); ?>
+			<?php esc_html_e( 'Enjoying Product Category Showcase for WooCommerce?', 'wc-category-showcase' ); ?>
 		</h3>
 		<p>
 			<?php
 			echo wp_kses_post(
 				sprintf(
-				// translators: %1$s: WC Category Showcase Pro link, %2$s: Coupon code.
+				// translators: %1$s: Product Category Showcase for WooCommerce plugin link, %2$s: Coupon code.
 					__( 'We hope you had a wonderful experience using %1$s. Please take a moment to show us your support by leaving a 5-star review on <a href="%2$s" target="_blank"><strong>WordPress.org</strong></a>. Thank you! 😊', 'wc-category-showcase' ),
-					'<a href="ttps://wordpress.org/plugins/wc-category-showcase/" target="_blank"><strong>WC Category Showcase</strong></a>',
+					'<a href="ttps://wordpress.org/plugins/wc-category-showcase/" target="_blank"><strong>Product Category Showcase for WooCommerce</strong></a>',
 					'https://wordpress.org/support/plugin/wc-category-showcase/reviews/?filter=5#new-post'
 				)
 			);
