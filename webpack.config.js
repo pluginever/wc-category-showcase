32edd56a13d8c39a59a45421d430d157b0ddf93b
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2025-12-14 16:35:12 +0600

    Update dev tools

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,62 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require("path");
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+/**
+ * External dependencies
+ */
+const baseConfig = require( '@byteever/scripts/config/webpack.config' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 
 module.exports = {
-	...defaultConfig,
-	entry: {
-		...defaultConfig.entry(),
-		'styles/admin': './resources/styles/admin.scss',
-		'styles/tailwind': './resources/styles/tailwind.scss',
-		'styles/frontend': './resources/styles/frontend.scss',
-		'styles/black-friday': './resources/styles/black-friday.scss',
-		'scripts/admin': './resources/scripts/admin.js',
-		'scripts/frontend': './resources/scripts/frontend.js',
-	},
-	resolve: {
-		...defaultConfig.resolve,
-		modules: ['node_modules'],
-	},
-	plugins: [
-		...defaultConfig.plugins,
+	...baseConfig,
 
-		/**
-		 * Copy source files/directories to a build directory.
-		 *
-		 * @see https://www.npmjs.com/package/copy-webpack-plugin
-		 */
-		new CopyWebpackPlugin({
+	entry: {
+		...baseConfig.entry,
+		// Styles
+		'styles/admin': './assets/src/styles/admin.scss',
+		'styles/tailwind': './assets/src/styles/tailwind.scss',
+		'styles/frontend': './assets/src/styles/frontend.scss',
+
+		// Scripts
+		'scripts/admin': './assets/src/scripts/admin.js',
+		'scripts/frontend': './assets/src/scripts/frontend.js',
+	},
+
+	optimization: {
+		...baseConfig.optimization,
+		splitChunks: {
+			...baseConfig.optimization.splitChunks,
+			cacheGroups: {
+				...baseConfig.optimization.splitChunks.cacheGroups,
+				styles: {
+					...baseConfig.optimization.splitChunks.cacheGroups.styles,
+					// Disable automatic CSS chunk splitting to prevent numbered files like 34.css
+					name: false,
+					minChunks: 999, // Effectively disable shared CSS extraction
+				},
+			},
+		},
+	},
+
+	plugins: [
+		...( baseConfig.plugins || [] ),
+
+		new CopyWebpackPlugin( {
 			patterns: [
 				{
-					from: 'images/*.{jpg,jpeg,png,gif,svg}',
-					to: 'images/[name][ext]',
-					context: 'resources',
-					noErrorOnMissing: true,
-				},
-				// Fonts in the src/fonts directory to the assets/fonts directory.
-				{
-					from: 'fonts/**/*.{woff,woff2,eot,ttf,otf}',
-					context: 'resources',
-					noErrorOnMissing: true,
-				},
-				// Copy the Splide slider related css files to the scripts directory.
-				{
-					from: './@splidejs/splide/dist/css/splide.min.css',
+					from: 'node_modules/@splidejs/splide/dist/css/splide.min.css',
 					to: 'styles/splide.min.css',
-					context: 'node_modules',
-					noErrorOnMissing: true,
 				},
-				// Copy the Splide slider related js files to the scripts directory.
 				{
-					from: './@splidejs/splide/dist/js/splide.js',
+					from: 'node_modules/@splidejs/splide/dist/js/splide.js',
 					to: 'scripts/splide.js',
-					context: 'node_modules',
-					noErrorOnMissing: true,
 				},
 				{
-					from: './@splidejs/splide-extension-grid/dist/js/splide-extension-grid.js',
+					from: 'node_modules/@splidejs/splide-extension-grid/dist/js/splide-extension-grid.js',
 					to: 'scripts/splide-extension-grid.js',
-					context: 'node_modules',
-					noErrorOnMissing: true,
 				},
 				{
-					from: './@splidejs/splide-extension-auto-scroll/dist/js/splide-extension-auto-scroll.js',
+					from: 'node_modules/@splidejs/splide-extension-auto-scroll/dist/js/splide-extension-auto-scroll.js',
 					to: 'scripts/splide-extension-auto-scroll.js',
-					context: 'node_modules',
-					noErrorOnMissing: true,
-				}
+				},
 			],
-		}),
-
-		/**
-		 * Remove empty scripts.
-		 *
-		 * @see https://www.npmjs.com/package/webpack-remove-empty-scripts
-		 */
-		new RemoveEmptyScriptsPlugin({
-			stage: RemoveEmptyScriptsPlugin.STAGE_AFTER_PROCESS_PLUGINS,
-			remove: /\.(js)$/,
-		}),
+		} ),
 	],
 };
